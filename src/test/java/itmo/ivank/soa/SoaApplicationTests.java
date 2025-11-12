d905d2663b1549337c9ee5129e4c530b0ab54bb0
tg <eat.some.trouble@gmail.com>
2025-11-12 18:10:05 +0300

    initial commit

---
+++ src/test/java/itmo/ivank/soa/SoaApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.ivank.soa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
