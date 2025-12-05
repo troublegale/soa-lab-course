a6bd1a715bdbfb1b3d9cf258a8ea37518e0a00e6
ivank <367292@edu.itmo.ru>
2025-12-05 03:56:18 +0300

    path var usage

--- jax-rs/src/main/java/itmo/ivank/OrgsResource.java
+++ jax-rs/src/main/java/itmo/ivank/OrgsResource.java
@@ -2,13 +2,12 @@ package itmo.ivank;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
-import javax.net.ssl.SSLContext;
 
 @Path("/orgs")
 public class OrgsResource {
@@ -20,6 +19,25 @@ public class OrgsResource {
     public String getOrganizations() {
         try (Client client = ClientBuilder.newClient()) {
             Response resp = client.target(SPRING_URL)
+                    .request()
+                    .header("Connection", "close")
+                    .get();
+            return resp.readEntity(String.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "<error>" + ex.getMessage() + "</error>";
+        }
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public String getOrganization(@PathParam("id") Long id) { // ← получаем id
+        try (Client client = ClientBuilder.newClient()) {
+            // Строим полный URL: .../organizations/{id}
+            String targetUrl = SPRING_URL + "/" + id;
+
+            Response resp = client.target(targetUrl)
                     .request()
                     .header("Connection", "close")
                     .get();
