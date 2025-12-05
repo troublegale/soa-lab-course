bcb0d0c78f5337cf6a01b17fef4872c08f644ed9
ivank <367292@edu.itmo.ru>
2025-12-05 22:17:05 +0300

    remove crazy annotation

--- spring/src/main/java/itmo/ivank/soa/repository/EmployeeRepository.java
+++ spring/src/main/java/itmo/ivank/soa/repository/EmployeeRepository.java
@@ -12,9 +12,6 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 
-    @Query(
-            "SELECT em FROM Employee em WHERE COALESCE(:name, NULL) IS NULL OR em.name ILIKE CONCAT('%', CAST(:name AS string ), '%') "
-    )
     List<Employee> findByOrganization(Organization organization);
 
 }
