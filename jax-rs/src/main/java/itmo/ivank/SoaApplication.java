679208e3c6a92b94059c754f05c3fafda3a43286
ivank <367293@edu.itmo.ru>
2026-01-26 11:33:41 +0300

    remove jax-rs

--- jax-rs/src/main/java/itmo/ivank/SoaApplication.java
+++
@@ -1,8 +0,0 @@
-package itmo.ivank;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api/v1")
-public class SoaApplication extends Application {
-}
--- jax-rs/src/main/java/itmo/ivank/exception/ApiException.java
+++
@@ -1,7 +0,0 @@
-package itmo.ivank.exception;
-
-public class ApiException extends RuntimeException {
-    public ApiException(String message) {
-        super(message);
-    }
-}
